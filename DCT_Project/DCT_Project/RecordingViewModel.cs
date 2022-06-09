e55639c40879f235e4b2f1b7e89463bce4d2f2ab
Roman Voskoboynikov <roma.voskoboynikov.78@gmail.com>
2022-06-09 14:25:05 +0300

    First UWP Project

---
+++ DCT_Project/DCT_Project/RecordingViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCT_Project
+{
+    public class RecordingViewModel
+    {
+        private ObservableCollection<Person> people = new ObservableCollection<Person>();
+        public ObservableCollection<Person> People { get { return this.people; } }
+
+        public RecordingViewModel()
+        {
+            this.people.Add(new Person() { FirstName = "Alina", LastName = "Kotkova" });
+            this.people.Add(new Person() { FirstName = "Sergey", LastName = "Korolenko" });
+            this.people.Add(new Person() { FirstName = "Sergey", LastName = "Kolotilo" });
+        }
+    }
+}
